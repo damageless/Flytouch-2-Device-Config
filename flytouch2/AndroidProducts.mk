9ab46bf080ceb8558edf0594e0ef4cc29b03d206
Bram Avontuur <bram@avontuur.org>
2011-01-23 16:43:54 +0100

    first checkin

---
+++ flytouch2/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/flytouch2.mk
---
+++ flytouch2/flytouch2.mk
@@ -0,0 +1,22 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+
+PRODUCT_NAME := flytouch2
+PRODUCT_MANUFACTURER := gome
+
+# XXX how do I add that app properly to the src tree?
+PRODUCT_PACKAGES += \
+		TSCalibration
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=ra0 \
+		wifi.supplicant_scan_interval=15 \
+		dalvik.vm.heapsize=32m
+
+PRODUCT_COPY_FILES := \
+    device/gome/flytouch2/init.rc:root/init.rc \
+		device/gome/flytouch2/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin
+
+TARGET_CPU_ABI = armeabi
+
+# Add tslib compilation
+TARGET_HAVE_TSLIB := true
