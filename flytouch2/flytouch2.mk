034ae169a5f8f04b9594978e88aaf68ef75300da
Bram Avontuur <bram@avontuur.org>
2011-01-26 23:45:15 +0100

    started audio implementation; integrated g-sensor axis swap settings

--- flytouch2/flytouch2.mk
+++ flytouch2/flytouch2.mk
@@ -7,13 +7,34 @@ PRODUCT_MANUFACTURER := gome
 PRODUCT_PACKAGES += \
     TSCalibration
 
+# checkjni: enable by eng build, but makes some apps fail
+# debug.sf.hw: Use GPU to render interface
+# scan_interval: only used when not in range of remembered access points. Saves
+#                battery life then
+# rild*: Use reference implementation which fakes all phone-related features
+# debug.sensors*: Used to swap the accelerometer, as it's misplaced at 90 degrees.
+#                 Some games break because of this, so they can be swapped back on 
+#                 demand by the user by monitoring the property every X milliseconds.
+minus := -
+
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=ra0 \
-    wifi.supplicant_scan_interval=15 \
-    dalvik.vm.heapsize=32m
+    wifi.supplicant_scan_interval=120 \
+    dalvik.vm.heapsize=32m \
+		rild.libpath=/system/lib/libreference-ril.so \
+		ro.kernel.android.checkjni=0 \
+		debug.sf.hw=1 \
+		debug.sensors.swap_mon_interval=3000 \
+		debug.sensors.swap_accel=$(subst _,$(minus),_y,x,z)
+
+# XXX these spaces will be converted to newlines, how to prevent?
+#		rild.libargs="-d /dev/ttyUSB2 -u /dev/ttyUSB0" \
 
 FT2RES := device/gome/flytouch2/resources
 
+# Copy files from original firmware. All of these are used to support the hardware.
+# Most of these files do not come with source, so until then, just copy the binaries
+# and hope it will work. OpenGL driver and kernel + modules do!
 PRODUCT_COPY_FILES := \
     $(FT2RES)/initlogo.rle:root/initlogo.rle \
     $(FT2RES)/init.rc:root/init.rc \
@@ -35,3 +56,8 @@ TARGET_CPU_ABI = armeabi
 
 # Add tslib compilation
 TARGET_HAVE_TSLIB := true
+# Add alsa. For this, make sure to checkout alsa-lib into external/ , and alsa_sound
+# into hardware/
+BOARD_USES_ALSA_AUDIO := true
+# For this, checkout alsa-utils into external/ 
+BUILD_WITH_ALSA_UTILS := true
